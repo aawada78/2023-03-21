b66dc79c52cdd0f119bcd587332f421353b59725
Assaad Awada <a_awada78@hotmail.com>
2023-03-22 23:04:08 +0100

    Lab Forms - prep

---
+++ src/app/shared/controls/flight-edit-extended/flight-edit-extended.component.ts
@@ -0,0 +1,83 @@
+import { JsonPipe, NgIf, NgStyle } from '@angular/common';
+import { inject } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+// src/app/flight-booking/flight-edit/flight-edit.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Flight } from '../../../flight-booking/flight';
+// import { validateCity, validateCityWithParams } from '../../validation/city-validator';
+import { PersonSubformComponent } from '../person/person.subform';
+import { Address } from '../address/address.control';
+
+@Component({
+  selector: 'app-flight-edit',
+  standalone: true,
+  templateUrl: './flight-edit-extended.component.html',
+  styleUrls: ['./flight-edit-extended.component.scss'],
+  imports: [NgStyle, NgIf, JsonPipe, ReactiveFormsModule, AddressControl, PersonSubformComponent]
+})
+export class FlightEditComponent implements OnInit {
+  id = 0;
+  showDetails = false;
+  carColor = '';
+  editForm = inject(NonNullableFormBuilder).group(
+    {
+      id: [0],
+      // from: ['Graz', [Validators.required, Validators.minLength(3), validateCity]],
+      from: ['Graz', [Validators.required, Validators.minLength(3)]],
+      // to: ['Hamburg', [validateCityWithParams(['Graz', 'Hamburg', 'Amsterdam'])]],
+      to: ['Hamburg', []],
+      date: [new Date().toISOString()],
+      delayed: [false],
+      address: [
+        {
+          street: '',
+          // eslint-disable-next-line id-blacklist
+          number: '',
+          zip: '',
+          city: '',
+          country: ''
+        } as Address
+      ],
+      person: new FormGroup({})
+    },
+    { updateOn: 'change' }
+  );
+  #route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.#route.params.subscribe((p) => {
+      this.editForm.patchValue({
+        id: p.id
+      });
+      this.showDetails = p.showDetails;
+      this.carColor = p.carColor;
+    });
+
+    this.editForm.controls.from.valueChanges.subscribe((value) => console.log('From value', value));
+
+    this.editForm.valueChanges.subscribe(console.log);
+  }
+
+  save(): void {
+    console.log('value', this.editForm.value);
+    console.log('valid', this.editForm.valid);
+    console.log('dirty', this.editForm.dirty);
+    console.log('touched', this.editForm.touched);
+
+    this.dispatch(this.editForm.getRawValue());
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  dispatch(flight: Flight): void {}
+
+  toggleDisableAddress(): void {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    this.editForm.controls.address.enabled ? this.editForm.controls.address.disable() : this.editForm.controls.address.enable();
+  }
+
+  reset(): void {
+    this.editForm.reset();
+  }
+}
