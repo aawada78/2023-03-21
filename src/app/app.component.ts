a3d1f56fa86b591bf25f7c1c249bbb25c7baacb3
Assaad Awada <assaadawada@AAs-MBP-2.local>
2023-03-23 13:35:20 +0100

    Lab Services - Injection tokens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { BASE_URL } from './app.module';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,14 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'Hello World!';
 
+  private baseUrl = inject(BASE_URL);
+
   /**
    *
    */
+  // constructor(@Inject(BASE_URL) private baseUrl: string) {
   constructor() {
+    console.log(this.baseUrl);
     const clickObs = fromEvent(document, 'click');
 
     const clickTime$ = clickObs.pipe(map((event) => `Event time ${event.timeStamp}`));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FlightBookingModule } from './flight-booking/flight-booking.module';
@@ -15,11 +15,28 @@ import { APP_ROUTES } from './app.routes';
 import { SharedModule } from './shared/shared.module';
 import { BasketComponent } from './basket/basket.component';
 import { CustomerModule } from './customer/customer.module';
+import { environment } from 'src/environments/environment';
+
+export const BASE_URL = new InjectionToken('BASE_URL', {
+  providedIn: 'root',
+  factory: () => {
+    if (environment.production) {
+      return 'https://www.bing.ch';
+    } else {
+      return 'https://www.google.ch';
+    }
+  }
+});
 
 @NgModule({
   imports: [CustomerModule, RouterModule.forRoot(APP_ROUTES), HttpClientModule, BrowserModule, FlightBookingModule, SharedModule],
   declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, AboutComponent, NotFoundComponent, BasketComponent],
-  providers: [],
+  providers: [
+    // {
+    //   provide: BASE_URL,
+    //   useValue: 'https://www.google.ch'
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
